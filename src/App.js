8e9894f0bc7254c260bd677bd729a1def5a56797
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-01-13 11:36:56 +0530

    created useEffect hook to fetch the questions and used useReducer to set the state and status of questions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 function App() {
+
+  const initialState = {
+    questions : [],
+
+    //"loading", 'ready', 'error', 'active', 'finished'
+    status: "loading",
+  };
   
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return { ...state, questions: action.payload, status: "ready" };
+      case 'dataFailed' : 
+      return {...state, status:"error"}
+      default:
+        throw new Error("Action Unknown");
+    }
+  }
+
+  const[state, dispatch] = useReducer(reducer, initialState);
+
+  //fetching data from the fake server
+  useEffect(function () {
+    fetch("http://localhost:3001/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({type:'dataReceived', payload : data }))
+      .catch((err) => dispatch({type: 'dataFailed'}));
+  });
+  
+  
+
   return (
     <div className="app">
      <Header />
